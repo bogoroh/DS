1706a680283645b5720114f35b31da78ff80fb07
bogoroh <mtaatgen@live.com>
2014-02-18 20:44:24 -0500

    A - Winspeed +  Direction

--- Project/index.php
+++ Project/index.php
@@ -1,45 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-if(isset($_POST["submit"])) {
-$json_string = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$_POST['city']);
-$parsed_json = json_decode($json_string);
-$tempK = $parsed_json->{'main'}->{'temp'};
-$tempMinK = $parsed_json->{'main'}->{'temp_min'};
-$tempMaxK = $parsed_json->{'main'}->{'temp_max'};
-$temp = round(($tempK * (9/5)) - 459.67);
-$tempMin = round(($tempMinK * (9/5)) - 459.67);
-$tempMax = round(($tempMaxK * (9/5)) - 459.67);
-$sunriseUnix = $parsed_json->{'sys'}->{'sunrise'};
-$sunsetUnix = $parsed_json->{'sys'}->{'sunset'};
-$sunrise = gmdate("D,d M Y H:i:s e", $sunriseUnix);
-$sunset = gmdate("D,d M Y H:i:s e", $sunsetUnix);
-$humidity = $parsed_json->{'main'}->{'humidity'};
-$weatherCon = $parsed_json->{'weather'}[0]->{'main'};
-$city = $parsed_json->{'name'};
-$country = $parsed_json->{'sys'}->{'country'};
-
-#echo $json_string;
-echo "Current temp $temp. Current Temp Min $tempMin. Current temp Max $tempMax.";
-echo "</br>";
-echo "The current humidity: $humidity%.";
-echo "</br>";
-echo " Sunrise starts at $sunrise";
-echo "</br>";
-echo " Sunset starts at $sunset";
-echo "</br>";
-echo " Current weather condition $weatherCon";
-echo "</br>";
-echo "The city: $city";
-echo "</br>";
-echo "The country: $country";
-
-#echo "Current temperature in ${name} is: ${temp_f}\n";
-
-}
-
-
-=======
 	if(isset($_POST["submit"])) {
 		$json_string = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$_POST['city']);
 		$parsed_json = json_decode($json_string);
@@ -57,6 +16,59 @@ echo "The country: $country";
 		$weatherCon = $parsed_json->{'weather'}[0]->{'main'};
 		$city = $parsed_json->{'name'};
 		$country = $parsed_json->{'sys'}->{'country'};
+    $deg = $parsed_json->{'wind'}->{'deg'};
+$windspeed = $parsed_json->{'wind'}->{'speed'};
+switch ($deg) {
+    case ($deg > 348.75  || $deg < 11.25 ):
+        $dir = "N";
+        break;
+    case ($deg < 33.75):
+        $dir = "NNE";
+        break;
+    case ($deg < 56.25):
+        $dir = "NE";
+        break;
+    case ($deg < 78.75):
+        $dir = "ENE";
+        break;
+    case ($deg < 101.25):
+        $dir = "E";
+        break;
+    case ($deg < 123.75):
+        $dir = "ESE";
+        break;
+    case ($deg < 146.25):
+        $dir = "SE";
+        break;
+  case ($deg < 168.75):
+    $dir = "SSE";
+    break;
+  case ($deg < 191.25):
+    $dir = "S";
+    break;   
+  case ($deg < 213.75):
+    $dir = "SSW";
+    break;   
+  case ($deg < 236.25):
+    $dir = "SW";
+    break;   
+  case ($deg < 258.75):
+    $dir = "WSW";
+    break;   
+  case ($deg < 281.25):
+    $dir = "W";
+    break;      
+  case ($deg < 303.75):
+    $dir = "WNW";
+    break;      
+  case ($deg < 326.25):
+    $dir = "NW";
+    break;      
+  case ($deg < 348.75):
+    $dir = "NNW";
+    break;      
+
+}
 
 		echo "Current temp $temp. Current Temp Min $tempMin. Current temp Max $tempMax.";
 		echo "</br>";
@@ -71,10 +83,11 @@ echo "The country: $country";
 		echo "The city: $city";
 		echo "</br>";
 		echo "The country: $country";
-
-		echo "Current temperature in ${name} is: ${temp_f}\n";
+    echo "</br>";
+    echo "The windspeed: $windspeed mps.";
+    echo "</br>";
+    echo "The direction: $dir";
 	}
->>>>>>> 55c8733892d3ce137768485f5c20401733c3b6bf
 ?>
 
 <!DOCTYPE html>
